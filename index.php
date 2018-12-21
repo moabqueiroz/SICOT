b114f07181ab72014e4e04f5b6a7c79a73a0e7f5
Moab <moab_queiroz@hotmail.com>
2018-12-21 15:15:58 -0300

    primeiro envio

---
+++ index.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<!-- Título do sistema-->
+	<title>Sistema Destra</title>
+	
+	<!-- meta tags-->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+   	
+</head>
+
+<body>
+	<div class="d_header">
+		<div class="container">
+			<div class="row">
+				
+			</div>
+
+
+		</div>
+	</div>
+	<div class="d_info">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1 class="d_info_titulo">Bem-vindo ao SICOT</h1>
+					<p class="d_info_subtitulo">Cadastro de Ocorrências de Trânsito de Caruaru</p>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<div class="menu">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-2">
+					
+					<a href="#">
+						<div class="d_cadastrar">
+						<i class="glyphicon glyphicon-file"></i>
+						<span>CADASTRAR</span> 
+						
+						</div>
+					</a>
+					
+					
+				</div>
+				<div class="col-sm-2">
+					
+					<a href="#">
+						<div class="d_visualizar">
+							<i class="glyphicon glyphicon-search"></i>
+							<span>VISUALIZAR</span> 
+
+						</div>
+					</a>
+
+				</div>
+				
+			</div>
+		</div>
+
+	</div>
+	<div class="d_servicos">
+		<div class="container">
+			<h2>Serviços e orientações</h2>
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="">
+				<p class="h3"><a href="#">Cultura e turismo</a></p>
+				<p class="small">Pontos culturais e turísticos, história, incentivos, cursos culturais</p>
+				</div>
+				<div class="">
+				<p class="h3"><a href="#">Dinheiro e imposto</a></p>
+				<p class="small">IPTU, ITBI, ISS, tarifa de água e lixo, dívida, indicadores, consumidor</p>
+				</div>
+				<div class="">
+				<p class="h3"><a href="#">Educação</a></p>
+				<p class="small">Matrícula, escola e creche, EJA, ano letivo, material escolar, merenda</p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="">
+				<p class="h3"><a href="#">Cultura e turismo</a></p>
+				<p class="small">Pontos culturais e turísticos, história, incentivos, cursos culturais</p>
+				</div>
+				<div class="">
+				<p class="h3"><a href="#">Dinheiro e imposto</a></p>
+				<p class="small">IPTU, ITBI, ISS, tarifa de água e lixo, dívida, indicadores, consumidor</p>
+				</div>
+				<div class="">
+				<p class="h3"><a href="#">Educação</a></p>
+				<p class="small">Matrícula, escola e creche, EJA, ano letivo, material escolar, merenda</p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="">
+				<p class="h3"><a href="#">Cultura e turismo</a></p>
+				<p class="small">Pontos culturais e turísticos, história, incentivos, cursos culturais</p>
+				</div>
+				<div class="">
+				<p class="h3"><a href="#">Dinheiro e imposto</a></p>
+				<p class="small">IPTU, ITBI, ISS, tarifa de água e lixo, dívida, indicadores, consumidor</p>
+				</div>
+				<div class="">
+				<p class="h3"><a href="#">Educação</a></p>
+				<p class="small">Matrícula, escola e creche, EJA, ano letivo, material escolar, merenda</p>
+				</div>
+			</div>
+		</div>
+		</div>
+
+	</div>
+	
+	<div class="footer_main">
+			
+
+	</div>
+
+
+	
+
+
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
